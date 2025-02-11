6dc44976ce359fc67d9e9f1877550fd1c960f905
Ivan Nevostruev <Nevostruev27is@ya.ru>
2025-02-11 12:48:47 +0300

    Первая итерация 6-го ТЗ. - жду замечаний, хорошего дня!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 import manager.*;
+import task.EpicTask;
+import task.SubTask;
+import task.Task;
 
 public class Main {
-    public static void main(String[] args) { }
+    public static void main(String[] args) {
+        TaskManager taskManager = new InMemoryTaskManager();
+
+        Task task1 = new Task("1", "11");
+        Task task2 = new Task("2", "22");
+        taskManager.addNewTask(task1);
+        taskManager.addNewTask(task2);
+        EpicTask epicTask1 = new EpicTask("Епик1", "Епик11");
+        EpicTask epicTask2 = new EpicTask("Епик2", "Епик22");
+        taskManager.addNewEpic(epicTask1);
+        taskManager.addNewEpic(epicTask2);
+        SubTask subTask1 = new SubTask("Саб1", "Саб11", epicTask1.getId());
+        SubTask subTask2 = new SubTask("Саб2", "Саб22", epicTask1.getId());
+        SubTask subTask3 = new SubTask("Саб3", "Саб33", epicTask1.getId());
+        taskManager.addNewSubTask(subTask1);
+        taskManager.addNewSubTask(subTask2);
+        taskManager.addNewSubTask(subTask3);
+
+        taskManager.getSubTask(subTask1.getId());
+        taskManager.getSubTask(subTask2.getId());
+        taskManager.getSubTask(subTask1.getId());
+        taskManager.getSubTask(subTask3.getId());
+        taskManager.getTask(task2.getId());
+        taskManager.getTask(task1.getId());
+        taskManager.getTask(task2.getId());
+        taskManager.getEpicTask(epicTask1.getId());
+        taskManager.getEpicTask(epicTask2.getId());
+
+        System.out.println(taskManager.getHistory());
+        taskManager.deleteEpic(epicTask1.getId());
+        System.out.println(taskManager.getHistory());
+    }
 }
--- test/manager/InMemoryHistoryManagerTest.java
+++ test/manager/InMemoryHistoryManagerTest.java
@@ -3,6 +3,7 @@ package manager;
 import task.Task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class InMemoryHistoryManagerTest {
@@ -12,30 +13,19 @@ public class InMemoryHistoryManagerTest {
     @BeforeEach
     void beforeEach() {
         historyManager = Managers.getDefaultHistoryManager();
-        task = new Task("Uborka", "Uborka v dome");
+        task = new Task(1, "Uborka", "Uborka v dome");
     }
 
     @Test
-    void addTaskInHistory() {
-        historyManager.addTaskInHistory(task);
+    void notEmptyBeforeAddTask() {
+        historyManager.add(task);
         assertFalse(historyManager.getHistory().isEmpty());
     }
 
     @Test
-    void saveOldDataInHistoryManager() {
-        historyManager.addTaskInHistory(task);
-        task = new Task(task.getId(),"Uborka2", "Uborka v dome2");
-        Task task2 = historyManager.getHistory().getFirst();
-        assertNotEquals(task.getName(), task2.getName(), "Не найдена нужная версия");
-        assertNotEquals(task.getTaskDescription(), task2.getTaskDescription(), "Не найдена нужная версия");
-
-    }
-
-    @Test
-    void maxElementsInHistory() {
-        for (int i = 0; i <= 11 ; i++) {
-            historyManager.addTaskInHistory(task);
-        }
-        assertEquals(10, historyManager.getHistory().size(), "Неверное количесвто элементов");
+    void notDublicateInHistory() {
+        historyManager.add(task);
+        historyManager.add(task);
+        assertEquals(1, historyManager.getHistory().size(), "Неверный результат");
     }
 }
