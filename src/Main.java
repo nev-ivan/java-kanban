0d5dd3dc5153ebfed49baad187f61e7ec3b259ad
Ivan Nevostruev <Nevostruev27is@ya.ru>
2025-03-12 11:55:47 +0300

    Первая итерация 8-го ТЗ. Правки до проверки(3)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,3 @@
-import manager.InMemoryTaskManager;
-import manager.TaskManager;
-import task.EpicTask;
-import task.SubTask;
-import task.Task;
-
-import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
