92d84ff8751a791a58334976383643cac4b0d4d0
Ivan Nevostruev <Nevostruev27is@ya.ru>
2025-03-12 11:54:34 +0300

    Первая итерация 8-го ТЗ. Правки до проверки(2)

--- src/manager/InMemoryTaskManager.java
+++ src/manager/InMemoryTaskManager.java
@@ -1,9 +1,18 @@
 package manager;
 
-import task.*;
+import task.EpicTask;
+import task.SubTask;
+import task.Task;
+import task.TaskStatus;
 
 import java.time.Duration;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class InMemoryTaskManager implements TaskManager {
 
